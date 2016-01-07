bc51c3ba05611869a79433c1accf2679cb05d037
Jonathan Villadolid <jonathan.villadolid@gmail.com>
2016-01-07 12:39:13 -0500

    CodeSniffer: add PHPDoc comments

--- functions/Class-SDES-Static.php
+++ functions/Class-SDES-Static.php
@@ -60,6 +60,9 @@ class SDES_Static
 	 ***********************/
 	/**
 	 * Returns the default even if the value in the database is null or whitespace.
+	 * @param string $value				The theme modification name to pass to get_theme_mod.
+	 * @param string $default_to		Default value to return.
+	 * @param string $get_theme_mod		Reference to the get_theme_mod function, or a mock for testing.
 	 */
 	public static function get_theme_mod_defaultIfEmpty( $value, $default_to, $get_theme_mod = 'get_theme_mod' ) {
 		$output = $get_theme_mod( $value, $default_to );  // Default if no value stored in database.
@@ -69,6 +72,7 @@ class SDES_Static
 
 	/**
 	 * Check if user is both logged in and has a given capability.
+	 * @param string $capability	A capability or role name to pass to current_user_can.
 	 */
 	public static function Is_UserLoggedIn_Can( $capability ) {
 		return is_user_logged_in() && current_user_can( $capability );
@@ -76,6 +80,8 @@ class SDES_Static
 
 	/**
 	 * Get a string of class names for a WP_Post object and optionally apply a filter.
+	 * @param object $wp_post	The WP_Post object whose classes will be retrieved.
+	 * @param string $filter_tag	A filter to pass to apply_filters.
 	 */
 	public static function Get_ClassNames( $wp_post, $filter_tag = '' ) {
 	    $classes = empty( $wp_post->classes ) ? array() : (array) $wp_post->classes;
@@ -84,14 +90,16 @@ class SDES_Static
 	}
 
 	/**
-	 * Generate a key for a post's navpill menu location. Used as the 'theme_location' by wp_nav_menu()'s $args parameter and by the "Currently set to:" in Customizer.
+	 * Generate a key for a post's navpill menu location.
+	 * Used as the 'theme_location' by wp_nav_menu()'s $args parameter and by the "Currently set to:" in Customizer.
 	 */
 	public static function the_locationKey_navpills() {
 		// Assume called within TheLoop.
 		return 'pg-' . the_title_attribute( array( 'echo' => false ) );
 	}
 	/**
-	 * Generate a value for a post's navpill menu location. This is the display text shown in "Menu Locations" and "Manage Locations".
+	 * Generate a value for a post's navpill menu location.
+	 * This is the display text shown in "Menu Locations" and "Manage Locations".
 	 */
 	public static function the_locationValue_navpills() {
 		// Assume called within TheLoop.
@@ -100,17 +108,19 @@ class SDES_Static
 
 
 
-	/** fallback_navpills_warning - Call from wp_nav_menu as the 'fallback_cb' for navpills locations.
+	/**
+	 * Fallback_navpills_warning - Call from wp_nav_menu as the 'fallback_cb' for navpills locations.
 	 *    Optionally show a warning for logged in users (if the navpills are missing).
-	 *  $args - Accepts $args array used by wp_nav_menu (merged with any default values), plus the standard following params:
+	 *
+	 * @param array  $args  Accepts $args array used by wp_nav_menu (merged with any default values), plus the standard following params:
 	 *  $args['echo'] - Standard echo param, output to stdout if true.
 	 *  $args['warn'] - Boolean flag to display admin message (default to true).
 	 *  $args['warn_message'] - Format string for warning message (where %1$s is expended to the 'theme_location').
 	 *
-	 *  Testing Overrides:
-	 *  $shouldWarn - Override login and capabilities check.
-	 *  $get_query_var - Override call to get_query_var.
-	 *  $esc_attr = Override the sanitize function provided by WordPress (used in testing).
+	 * Testing Overrides.
+	 * @param bool   $shouldWarn		Override login and capabilities check.
+	 * @param string $get_query_var		Call to get_query_var.
+	 * @param string $esc_attr			Override the sanitize function provided by WordPress (used in testing).
 	 */
 	public static function fallback_navpills_warning( $args,
 		$shouldWarn = null, $get_query_var = 'get_query_var', $esc_attr = 'esc_attr' ) {
@@ -141,20 +151,22 @@ class SDES_Static
 		return $nav_menu;
 	}
 
-	/** fallback_navbar_list_pages - Call from wp_nav_menu as the 'fallback_cb'.
+	/**
+	 * Fallback_navbar_list_pages - Call from wp_nav_menu as the 'fallback_cb'.
 	 *   Allow graceful failure when menu is not set by showing a formatted listing of pages
 	 *   instead of the default wp_page_menu output.
-	 *  $args - Accepts $args array used by wp_nav_menu (merged with any default values), plus the standard following params:
+	 *
+	 *  @param array  $args	 Accepts $args array used by wp_nav_menu (merged with any default values), plus the standard following params:
 	 *  $args['number'] - Number of pages to pull from wp_list_pages.
 	 *  $args['echo'] - Standard echo param, output to stdout if true.
 	 *  $args['warn'] - Boolean flag to display admin message (default to true).
 	 *  $args['warn_message'] - Format string for warning message (where %1$s is expended to the 'theme_location').
 	 *
-	 *  Testing Overrides:
-	 *  $shouldWarn - Override login and capabilities check.
-	 *  $get_query_var - Override call to get_query_var.
-	 *  $wp_list_pages - Override the call to wp_list_pages (Returns a string containing li>a elements).
-	 *  $esc_attr = Override the sanitize function provided by WordPress (used in testing).
+	 *  Testing Overrides.
+	 *  @param bool   $shouldWarn - Override login and capabilities check.
+	 *  @param string $get_query_var	Override call to get_query_var.
+	 *  @param string $wp_list_pages	Override the call to wp_list_pages (Returns a string containing li>a elements).
+	 *  @param string $esc_attr			Override the sanitize function provided by WordPress (used in testing).
 	 */
 	public static function fallback_navbar_list_pages( $args,
 		$shouldWarn = null, $get_query_var = 'get_query_var', $wp_list_pages = 'wp_list_pages', $esc_attr = 'esc_attr' ) {
