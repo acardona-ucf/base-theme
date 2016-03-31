4ac8bfd4fd1a131ebd6321b846a56dedeb59fa95
Jonathan Villadolid <jonathan.villadolid@gmail.com>
2016-03-30 20:36:10 -0400

    CPT: m. ran phpcbf, added comments

--- functions/class-custom-posttype.php
+++ functions/class-custom-posttype.php
@@ -2,17 +2,20 @@
 /**
  * Abstract base class for creating custom posttypes.
  * This class is central the a theme's fuctionality, and heavily relies on other files.
+ *
+ * @package CustomPostType
  * @filesource
  */
+
 namespace SDES;
 
 require_once( get_stylesheet_directory().'/functions/class-sdes-metaboxes.php' );
 	use SDES\SDES_Metaboxes;
-	class_alias('SDES\\SDES_Metaboxes', 'SDES_Metaboxes');  // Allows calling class_exists() without any changes.
+	class_alias( 'SDES\\SDES_Metaboxes', 'SDES_Metaboxes' );  // Allows calling class_exists() without any changes.
 
 require_once( get_stylesheet_directory().'/functions/class-shortcodebase.php' );
 	use SDES\Shortcodes\ShortcodeBase;
-	class_alias('SDES\\Shortcodes\\ShortcodeBase', 'ShortcodeBase');
+	class_alias( 'SDES\\Shortcodes\\ShortcodeBase', 'ShortcodeBase' );
 
 require_once( get_stylesheet_directory().'/functions/class-sdes-static.php' );
 	use SDES\SDES_Static as SDES_Static;
@@ -30,45 +33,45 @@ use Underscore\Types\Arrays;
  * Based on: https://github.com/UCF/Students-Theme/blob/6ca1d02b062b2ee8df62c0602adb60c2c5036867/custom-post-types.php#L1-L242
  **/
 abstract class CustomPostType {
-	public
-		$name           = 'custom_post_type',
-		$plural_name    = 'Custom Posts',
-		$singular_name  = 'Custom Post',
-		$add_new_item   = 'Add New Custom Post',
-		$edit_item      = 'Edit Custom Post',
-		$new_item       = 'New Custom Post',
-		$public         = True,  // I dunno...leave it true
-		$use_title      = True,  // Title field
-		$use_editor     = True,  // WYSIWYG editor, post content field
-		$use_revisions  = True,  // Revisions on post content and titles
-		$use_thumbnails = False, // Featured images
-		$use_order      = False, // Wordpress built-in order meta data
-		$use_metabox    = False, // Enable if you have custom fields to display in admin
-		$use_shortcode  = False, // Auto generate a shortcode for the post type
-								 // (see also objectsToHTML and toHTML methods)
-		$taxonomies     = array( 'post_tag' ),
-		$built_in       = False,
-		// Optional default ordering for generic shortcode if not specified by user.
-		$default_orderby = null,
-		$default_order   = null,
-		// Interface Columns/Fields
-		$calculated_columns = array(
-			array( 'heading'=>'Thumbnail', 'column_name'=>'_thumbnail_id', 'custom_column_order'=>100 ),
-			// array( 'heading'=>'A Column Heading Text', 'column_name'=>'id_of_the_column' ),
-		), // Calculate values within custom_column_echo_data.
-		$sc_interface_fields = null; // Fields for shortcodebase interface (false hides from list, null shows only the default fields).
+  public
+	$name           = 'custom_post_type',
+	$plural_name    = 'Custom Posts',
+	$singular_name  = 'Custom Post',
+	$add_new_item   = 'Add New Custom Post',
+	$edit_item      = 'Edit Custom Post',
+	$new_item       = 'New Custom Post',
+	$public         = true,  // I dunno...leave it true
+	$use_title      = true,  // Title field
+	$use_editor     = true,  // WYSIWYG editor, post content field
+	$use_revisions  = true,  // Revisions on post content and titles
+	$use_thumbnails = false, // Featured images
+	$use_order      = false, // Wordpress built-in order meta data
+	$use_metabox    = false, // Enable if you have custom fields to display in admin
+	$use_shortcode  = false, // Auto generate a shortcode for the post type
+							 // (see also objectsToHTML and toHTML methods)
+	$taxonomies     = array( 'post_tag' ),
+	$built_in       = false,
+	// Optional default ordering for generic shortcode if not specified by user.
+	$default_orderby = null,
+	$default_order   = null,
+	// Interface Columns/Fields.
+	$calculated_columns = array(
+		array( 'heading' => 'Thumbnail', 'column_name' => '_thumbnail_id', 'custom_column_order' => 100 ),
+		// array( 'heading'=>'A Column Heading Text', 'column_name'=>'id_of_the_column' ),
+	), // Calculate values within custom_column_echo_data.
+	$sc_interface_fields = null; // Fields for shortcodebase interface (false hides from list, null shows only the default fields).
 
 	/**
 	 * Wrapper for get_posts function, that predefines post_type for this
 	 * custom post type.  Any options valid in get_posts can be passed as an
 	 * option array.  Returns an array of objects.
 	 * */
-	public function get_objects( $options=array() ) {
+	public function get_objects( $options = array() ) {
 		$defaults = array(
-			'numberposts'   => -1,
-			'orderby'       => 'title',
-			'order'         => 'ASC',
-			'post_type'     => $this->options( 'name' ),
+		'numberposts'   => -1,
+		'orderby'       => 'title',
+		'order'         => 'ASC',
+		'post_type'     => $this->options( 'name' ),
 		);
 		$options = array_merge( $defaults, $options );
 		$objects = get_posts( $options );
@@ -83,12 +86,12 @@ abstract class CustomPostType {
 		$objects = $this->get_objects( $options );
 		$opt     = array();
 		foreach ( $objects as $o ) {
-			switch ( True ) {
-			case $this->options( 'use_title' ):
-				$opt[$o->post_title] = $o->ID;
+			switch ( true ) {
+				case $this->options( 'use_title' ):
+					$opt[ $o->post_title ] = $o->ID;
 				break;
-			default:
-				$opt[$o->ID] = $o->ID;
+				default:
+					$opt[ $o->ID ] = $o->ID;
 				break;
 			}
 		}
@@ -100,7 +103,7 @@ abstract class CustomPostType {
 	 * */
 	public function options( $key ) {
 		$vars = get_object_vars( $this );
-		return $vars[$key];
+		return $vars[ $key ];
 	}
 
 	/**
@@ -115,9 +118,11 @@ abstract class CustomPostType {
 	/**
 	 * Using instance variables defined, returns an array defining what this
 	 * custom post type supports.
+	 * @see http://codex.wordpress.org/Function_Reference/register_post_type#supports Codex: register_post_type()
+	 * @see http://codex.wordpress.org/Function_Reference/add_post_type_support Codex: add_post_type_support()
 	 * */
 	public function supports() {
-		// Default support array
+		// Default support array.
 		$supports = array();
 		if ( $this->options( 'use_title' ) ) {
 			$supports[] = 'title';
@@ -139,14 +144,15 @@ abstract class CustomPostType {
 
 	/**
 	 * Creates labels array, defining names for admin panel.
+	 * @see http://codex.wordpress.org/Function_Reference/register_post_type#labels Codex: register_post_type()
 	 * */
 	public function labels() {
 		return array(
-			'name'          => __( $this->options( 'plural_name' ) ),
-			'singular_name' => __( $this->options( 'singular_name' ) ),
-			'add_new_item'  => __( $this->options( 'add_new_item' ) ),
-			'edit_item'     => __( $this->options( 'edit_item' ) ),
-			'new_item'      => __( $this->options( 'new_item' ) ),
+		'name'          => __( $this->options( 'plural_name' ) ),
+		'singular_name' => __( $this->options( 'singular_name' ) ),
+		'add_new_item'  => __( $this->options( 'add_new_item' ) ),
+		'edit_item'     => __( $this->options( 'edit_item' ) ),
+		'new_item'      => __( $this->options( 'new_item' ) ),
 		);
 	}
 
@@ -157,12 +163,12 @@ abstract class CustomPostType {
 	public function metabox() {
 		if ( $this->options( 'use_metabox' ) ) {
 			return array(
-				'id'       => 'custom_'.$this->options( 'name' ).'_metabox',
-				'title'    => __( $this->options( 'singular_name' ).' Fields' ),
-				'page'     => $this->options( 'name' ),
-				'context'  => 'normal',
-				'priority' => 'high',
-				'fields'   => $this->fields(),
+			'id'       => 'custom_'.$this->options( 'name' ).'_metabox',
+			'title'    => __( $this->options( 'singular_name' ).' Fields' ),
+			'page'     => $this->options( 'name' ),
+			'context'  => 'normal',
+			'priority' => 'high',
+			'fields'   => $this->fields(),
 			);
 		}
 		return null;
@@ -191,7 +197,7 @@ abstract class CustomPostType {
 	 * @see CustomPostType::do_meta_boxes_after_title()
 	 */
 	public static function register_meta_boxes_after_title() {
-		add_action('edit_form_after_title', __CLASS__.'::do_meta_boxes_after_title');
+		add_action( 'edit_form_after_title', __CLASS__.'::do_meta_boxes_after_title' );
 	}
 
 	/**
@@ -199,7 +205,7 @@ abstract class CustomPostType {
 	 * @see CustomPostType::register_meta_boxes_after_title()
 	 */
 	public static function do_meta_boxes_after_title( $post ) {
-		//global $post, $wp_meta_boxes; // Get the globals.
+		// global $post, $wp_meta_boxes; // Get the globals.
 		do_meta_boxes( get_current_screen(), 'after_title', $post ); // Output meta boxes for the 'after_title' context.
 	}
 
@@ -243,11 +249,11 @@ abstract class CustomPostType {
 		$calculated_columns = $this->options( 'calculated_columns' );
 		// Filter and Map fields to custom columns.
 		$field_columns = Arrays::from( $this->fields() )
-			->filter( function( $x ) { return array_key_exists( 'custom_column_order', $x ) && $x['custom_column_order']; } )
-			->each( function( $field ) {
-				$custom_column_order = isset( $field['custom_column_order'] ) ? $field['custom_column_order'] : 1000;
-				return array( 'heading' => $field['name'], 'column_name' => $field['id'], 'custom_column_order' => $custom_column_order );
-			})->obtain();
+		->filter( function( $x ) { return array_key_exists( 'custom_column_order', $x ) && $x['custom_column_order']; } )
+		->each( function( $field ) {
+			$custom_column_order = isset( $field['custom_column_order'] ) ? $field['custom_column_order'] : 1000;
+			return array( 'heading' => $field['name'], 'column_name' => $field['id'], 'custom_column_order' => $custom_column_order );
+		})->obtain();
 		$columns = Arrays::sort( array_merge( $calculated_columns, $field_columns ), 'custom_column_order' );
 		return $columns;
 	}
@@ -303,8 +309,8 @@ abstract class CustomPostType {
 	/**
 	 * Registers the custom post type and any other ancillary actions that are
 	 * required for the post to function properly.
-	 * @see http://codex.wordpress.org/Function_Reference/register_post_type Codex - register_post_type()
-	 * @see http://codex.wordpress.org/Function_Reference/add_shortcode Codex - add_shortcode()
+	 * @see http://codex.wordpress.org/Function_Reference/register_post_type Codex: register_post_type()
+	 * @see http://codex.wordpress.org/Function_Reference/add_shortcode Codex: add_shortcode()
 	 * @uses labels(), supports(), options(), custom_columns_get_all(), register_custom_columns()
 	 * @uses $public, $taxonomies, $built_in, $use_order, $name, $use_shortcode
 	 * @param Array $args Override the registration args passed to register_post_type.
@@ -312,11 +318,11 @@ abstract class CustomPostType {
 	public function register( $args = array() ) {
 		// Register the post type.
 		$registration = array(
-			'labels'     => $this->labels(),
-			'supports'   => $this->supports(),
-			'public'     => $this->options( 'public' ),
-			'taxonomies' => $this->options( 'taxonomies' ),
-			'_builtin'   => $this->options( 'built_in' ),
+		'labels'     => $this->labels(),
+		'supports'   => $this->supports(),
+		'public'     => $this->options( 'public' ),
+		'taxonomies' => $this->options( 'taxonomies' ),
+		'_builtin'   => $this->options( 'built_in' ),
 		);
 		if ( $this->options( 'use_order' ) ) {
 			$registration = array_merge( $registration, array( 'hierarchical' => true ) );
@@ -336,30 +342,34 @@ abstract class CustomPostType {
 	 * Shortcode for this custom post type.  Can be overridden for descendants.
 	 * Defaults to just outputting a list of objects outputted as defined by
 	 * toHTML method.
-	 * @see CustomPostType::objectsToHTML
-	 * @see CustomPostType::toHTML
+	 * @see SDES_Static::sc_object_list sc_object_list()
+	 * @see CustomPostType::objectsToHTML objectsToHTML()
+	 * @see CustomPostType::toHTML toHTML()
+	 * @uses SDES_Static::sc_object_list()
 	 * */
 	public function shortcode( $attr ) {
 		$default = array(
-			'type' => $this->options( 'name' ),
+		'type' => $this->options( 'name' ),
 		);
 		if ( is_array( $attr ) ) {
 			$attr = array_merge( $default, $attr );
-		}else {
+		} else {
 			$attr = $default;
 		}
-		$args = array( 'classname' => __CLASS__, );
+		$args = array( 'classname' => __CLASS__ );
 		return SDES_Static::sc_object_list( $attr, $args );
 	}
 
 	/**
 	 * Static method that tries to call the correct instance method of objectsToHTML.
-	 * @param string $classname Override the classname to instantiate the class.
+	 * @param WP_Post $objects    Iterable of the post objects to display.
+	 * @param string  $css_classes List of css classes for the objects container.
+	 * @param string  $classname   Override the classname to instantiate the class.
 	 */
 	public static function tryObjectsToHTML( $objects, $css_classes, $classname = '' ) {
 		if ( count( $objects ) < 1 ) { return (WP_DEBUG) ? '<!-- No objects were provided to objectsToHTML. -->' : '';}
 		$classname = ( '' === $classname ) ? $objects[0]->post_type : $classname;
-		if( class_exists($classname) ) {
+		if ( class_exists( $classname ) ) {
 			$class = new $classname;
 			return $class->objectsToHTML( $objects, $css_classes );
 		} else { return ''; }
@@ -370,15 +380,15 @@ abstract class CustomPostType {
 	 * If you want to override how a list of objects are outputted, override
 	 * this, if you just want to override how a single object is outputted, see
 	 * the toHTML method.
-	 * @param WP_Post $objects The post objects to display.
-	 * @param string $css_classes List of css classes for the objects container.
+	 * @param WP_Post $objects Iterable of the post objects to display.
+	 * @param string  $css_classes List of css classes for the objects container.
 	 * @see http://php.net/manual/en/language.oop5.late-static-bindings.php
 	 * @see Always prefer `static::` over `self::` (in PHP 5.3+): http://stackoverflow.com/a/6807615
 	 * */
 	public function objectsToHTML( $objects, $css_classes ) {
 		if ( count( $objects ) < 1 ) { return (WP_DEBUG) ? '<!-- No objects were provided to objectsToHTML. -->' : '';}
 		$context['objects'] = $objects;
-		$context['css_classes'] = ( $css_classes ) ? $css_classes : $this->options('name').'-list';
+		$context['css_classes'] = ( $css_classes ) ? $css_classes : $this->options( 'name' ).'-list';
 		return static::render_objects_to_html( $context );
 	}
 
@@ -386,19 +396,19 @@ abstract class CustomPostType {
 	 * Render HTML for a collection of objects.
 	 * @param Array $context An array of sanitized variables to display with this view.
 	 */
-	protected static function render_objects_to_html( $context ){
+	protected static function render_objects_to_html( $context ) {
 		ob_start();
 		?>
 		<ul class="<?= $context['css_classes'] ?>">
-			<?php foreach ( $context['objects'] as $o ):?>
-			<li>
-				<?= static::toHTML( $o ) ?>
-			</li>
-			<?php endforeach;?>
-		</ul>
-		<?php
-		$html = ob_get_clean();
-		return $html;
+		<?php foreach ( $context['objects'] as $o ) : ?>
+		<li>
+			<?= static::toHTML( $o ) ?>
+		</li>
+		<?php endforeach;?>
+	</ul>
+	<?php
+	$html = ob_get_clean();
+	return $html;
 	}
 
 	/**
@@ -418,56 +428,58 @@ abstract class CustomPostType {
 	protected static function render_to_html( $context ) {
 		ob_start();
 		?>
-			<a href="<?= $context['permalink'] ?>"><?= $context['post_title'] ?></a>
-		<?php
-		$html = ob_get_clean();
-		return $html;
+		<a href="<?= $context['permalink'] ?>"><?= $context['post_title'] ?></a>
+	<?php
+	$html = ob_get_clean();
+	return $html;
 	}
 
 
 	/**
+	 * Register the custom post types wiht WordPress and auxilliary classes (should be called from custom-posttypes.php).
 	 * @param Array $custom_posttypes Names of custom post types classes to register.
 	 * @return  Array Array of instantiated posttype classes (array of arrays). Each item has the keys: 'classname', 'instance'.
 	 * @see SDES_Static::instantiate_and_register_classes()
 	 */
 	public static function Register_Posttypes( $custom_posttypes ) {
-		$posttype_instances = SDES_Static::instantiate_and_register_classes($custom_posttypes);
-		foreach ($posttype_instances as $registered_posttype) {
-			if (class_exists('SDES_Metaboxes') ) {
+		$posttype_instances = SDES_Static::instantiate_and_register_classes( $custom_posttypes );
+		foreach ( $posttype_instances as $registered_posttype ) {
+			if ( class_exists( 'SDES_Metaboxes' ) ) {
 				SDES_Metaboxes::$installed_custom_post_types[] = $registered_posttype['instance'];
 			}
-			if (class_exists('ShortcodeBase') ) {
+			if ( class_exists( 'ShortcodeBase' ) ) {
 				ShortcodeBase::$installed_custom_post_types[] = $registered_posttype['instance'];
 			}
 		}
-		static::Register_Thumbnails_Support($posttype_instances);
+		static::Register_Thumbnails_Support( $posttype_instances );
 		return $posttype_instances;
 	}
 
 	/**
+	 * Register theme support for any custom post_types with $this->use_thumbnails set to true.
 	 * @param Array $instances Instantiated classes for Custom Post Types.
+	 * @see http://codex.wordpress.org/Function_Reference/add_theme_support Codex: add_theme_support
 	 * @return void
 	 */
 	public static function Register_Thumbnails_Support( $instances ) {
-		// if the key $instances[0]['instance'] exists.
-		if ( Arrays::has(Object::unpack($instances), 'instance') ) {
-			$instances = Arrays::pluck($instances, 'instance');
+		// If the key $instances[0]['instance'] exists.
+		if ( Arrays::has( Object::unpack( $instances ), 'instance' ) ) {
+			$instances = Arrays::pluck( $instances, 'instance' );
 		}
 
 		$thumbnail_posttypes
-		  = Arrays::from($instances)
-			->filter( function($x) { return true === $x->use_thumbnails; })
-			->pluck( 'name' )
-			->obtain();
+		  = Arrays::from( $instances )
+		->filter( function( $x ) { return true === $x->use_thumbnails; } )
+		->pluck( 'name' )
+		->obtain();
 		add_theme_support( 'post-thumbnails', $thumbnail_posttypes );
 
-		/** Scale thumbnails by default. */ define('SDES\\SCALE', false); 
-		/** Crop thumbnails by default. */  define('SDES\\CROP', true);
-		// For cropping behavior see `add_image_size`, e.g.: https://core.trac.wordpress.org/browser/tags/4.4.2/src/wp-includes/media.php#L228
+		/** Scale thumbnails by default. */ define( 'SDES\\SCALE', false );
+		/** Crop thumbnails by default. */  define( 'SDES\\CROP', true );
+		// For cropping behavior see `add_image_size`, e.g.: https://core.trac.wordpress.org/browser/tags/4.4.2/src/wp-includes/media.php#L228 .
 		set_post_thumbnail_size( 125, 125, CROP );
 		// $crop_from = array( 'top', 'left');
 		// set_post_thumbnail_size( 125, 125, $crop_from );
-
 	}
 }
 
